607f23d44139b12b8e45382cfdd20221a8b39c19
Zcounts <zacounts@gmail.com>
2025-03-19 18:53:27 -0500

    Update init-py-files.py

--- init-py-files.py
+++ init-py-files.py
@@ -3,6 +3,10 @@
 
 __version__ = '0.1.0'
 
+"""Data models for the LightCraft application."""
+"""Controllers for the LightCraft application."""
+"""UI components for the LightCraft application."""
+
 # lightcraft/ui/__init__.py
 """UI components for the LightCraft application."""
 
