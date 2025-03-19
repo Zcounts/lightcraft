52f3a45ba3cdd3f0d8c16e55d0d0f2d66e2e15aa
Zcounts <zacounts@gmail.com>
2025-03-19 16:21:03 -0500

    Create __init__.py

---
+++ lightcraft/models/__init__.py
@@ -0,0 +1,17 @@
+"""
+Content for __init__.py files
+"""
+
+# lightcraft/__init__.py
+"""LightCraft - Lighting Diagram Designer for Film Shoots."""
+
+__version__ = '0.1.0'
+
+# lightcraft/models/__init__.py
+"""Data models for the LightCraft application."""
+
+# lightcraft/controllers/__init__.py
+"""Controllers for the LightCraft application."""
+
+# lightcraft/ui/__init__.py
+"""UI components for the LightCraft application."""
